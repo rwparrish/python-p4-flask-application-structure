7fc07bf8d11779979d0b125e14b53c3758ec27d8
rwparrish <60946750+rwparrish@users.noreply.github.com>
2022-11-17 10:51:22 -0700

    notes included

--- app/app.py
+++ app/app.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
+# app.route is a decorator that registers index and user functions with an instance of the Flask app
+# app.route is a decorator that modifies the the instance of the app thereby directing requets to routes to the the specifed show function
 
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=5556)
